7ba5ef17d45639d7507bbd99be058a17319fab6a
SusuTawar <realsusutawar@gmail.com>
2024-05-10 21:04:47 +0700

    fix: wrong phpdoc

--- src/Facade/Whatsapp.php
+++ src/Facade/Whatsapp.php
@@ -7,7 +7,12 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Dekasender Whatsapp Facade
  *
- * @method static \Dekasender\HttpClient\Response sendEmail(string $receiver, string $subject, string $message, string $sender, string $replyTo)
+ * @method static \Dekasender\HttpClient\Response sendMessage(string $receiver, string $message)
+ * @method static \Dekasender\HttpClient\Response sendFile(string $receiver, string $imageUrl, string $type, string $caption = "")
+ * @method static \Dekasender\HttpClient\Response sendImage(string $receiver, string $imageUrl, string $caption = "")
+ * @method static \Dekasender\HttpClient\Response sendAudio(string $receiver, string $audioUrl, string $caption = "")
+ * @method static \Dekasender\HttpClient\Response sendVideo(string $receiver, string $videoUrl, string $caption = "")
+ * @method static \Dekasender\HttpClient\Response sendDocument(string $receiver, string $documentUrl, string $caption = "")
  */
 class Whatsapp extends Facade
 {
